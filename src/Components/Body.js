40b0664e7beb638bf2dd568613d161f34e99d241
Rishi-Mohan <jharishimohan@gmail.com>
2023-06-22 01:08:58 +0530

    useState Working

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,41 +1,41 @@
 import RestaurentCard from "./RestaurentCard";
-// not using for now, creating own small dataSet
-// import resList from "../utils/mockData";
-
-let listOfRestaurent = [
-  {
-    data: {
-      id: "252401",
-      name: "Aazhi Seafood Restaurant",
-      cloudinaryImageId: "ddmwtfzxljkxoju5vwxg",
-      cuisines: ["Chettinad", "Seafood"],
-      costForTwo: 40000,
-      avgRating: "3.9",
-    },
-  },
-  {
-    data: {
-      id: "252402",
-      name: "xyz",
-      cloudinaryImageId: "ddmwtfzxljkxoju5vwxg",
-      cuisines: ["Chettinad", "Seafood"],
-      costForTwo: 40000,
-      avgRating: "4.2",
-    },
-  },
-  {
-    data: {
-      id: "252403",
-      name: "abc",
-      cloudinaryImageId: "ddmwtfzxljkxoju5vwxg",
-      cuisines: ["Chettinad", "Seafood"],
-      costForTwo: 40000,
-      avgRating: "4.1",
-    },
-  },
-];
+import { useState } from "react";
 
 const Body = () => {
+  // Super Powerful JS variable aka State Variable
+  const [listOfRestaurent, setlistOfRestaurent] = useState([
+    {
+      data: {
+        id: "252401",
+        name: "Aazhi Seafood Restaurant",
+        cloudinaryImageId: "ddmwtfzxljkxoju5vwxg",
+        cuisines: ["Chettinad", "Seafood"],
+        costForTwo: 40000,
+        avgRating: "3.9",
+      },
+    },
+    {
+      data: {
+        id: "252402",
+        name: "xyz",
+        cloudinaryImageId: "ddmwtfzxljkxoju5vwxg",
+        cuisines: ["Chettinad", "Seafood"],
+        costForTwo: 40000,
+        avgRating: "4.2",
+      },
+    },
+    {
+      data: {
+        id: "252403",
+        name: "abc",
+        cloudinaryImageId: "ddmwtfzxljkxoju5vwxg",
+        cuisines: ["Chettinad", "Seafood"],
+        costForTwo: 40000,
+        avgRating: "4.1",
+      },
+    },
+  ]);
+
   return (
     <div className="body">
       <div className="filter">
@@ -43,10 +43,10 @@ const Body = () => {
           className="filter-btn"
           onClick={() => {
             //filter logic here
-            listOfRestaurent = listOfRestaurent.filter(
+            const filteredlist = listOfRestaurent.filter(
               (res) => res.data.avgRating > 4
             );
-            console.log(listOfRestaurent);
+            setlistOfRestaurent(filteredlist);
           }}
         >
           Top Rated Restaurent
