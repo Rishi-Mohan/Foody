5a0e6f9c92cef87cfd9a820ec827f1e51400918c
Rishi-Mohan <jharishimohan@gmail.com>
2023-06-21 22:16:41 +0530

    source folder

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const LOGO_URL =
+  "https://www.logoground.com/uploads8/dv8y2020646782020-03-243826278Quick-resto2.jpeg";
